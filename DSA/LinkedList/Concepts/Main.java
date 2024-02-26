5d909d97ec0db77497ad382b2286039790bd0934
moharoon11 <moharoon11107@gmail.com>
2024-02-27 01:59:13 +0530

    Single and doubly linked list aroung 1.45am

--- DSA/LinkedList/Concepts/Main.java
+++ DSA/LinkedList/Concepts/Main.java
@@ -4,16 +4,16 @@ public class Main {
 
     public static void main(String... args) {
 
-<<<<<<< HEAD
-//        LL ll = new LL();
-//
-//       ll.insertLast(1);
-//       ll.insertLast(2);
-//       ll.insertLast(3);
-//
-//       ll.display();
-//       ll.delete(1);
-//       ll.display();
+
+        LL ll = new LL();
+
+       ll.insertLast(1);
+       ll.insertLast(2);
+       ll.insertLast(3);
+
+       ll.display();
+       ll.delete(1);
+       ll.display();
 
         DLL dll = new DLL();
         dll.insertFirst(3);
@@ -25,21 +25,8 @@ public class Main {
         dll.display();
 
 
-=======
-        LL ll = new LL();
 
-        ll.insertFirst(50);
-        ll.insertFirst(40);
-        ll.insertFirst(20);
-        ll.insertFirst(10);
-        ll.insertLast(60);
-        ll.insertLast(70);
-        ll.insertFirst(0);
-        ll.insert(30, 3);
-    
 
-        ll.display();
->>>>>>> origin/main
 
     }
     
