042799a90333e68b42de8786f5ab4eb616beaca8
moharoon11 <moharoon11107@gmail.com>
2024-03-04 00:22:16 +0530

    Segement tree

---
+++ DSA/Trees/SegmentTree.java
@@ -0,0 +1,69 @@
+package Trees;
+
+public class SegmentTree {
+
+    public static void main(String... args) {
+
+        int[] arr = {3, 8, 7, 6, -2, -8, 4, 9};
+        SegmentTree tree = new SegmentTree(arr);
+        tree.display();
+    }
+
+    private static class Node {
+
+        private int data;
+        private int startInterval;
+        private int endInterval;
+        private Node left;
+        private Node right;
+
+        Node(int startInterval, int endInterval) {
+            this.startInterval = startInterval;
+            this.endInterval = endInterval;
+        }
+
+    }
+
+    private Node root;
+
+    public SegmentTree(int[] arr) {
+        root = construct(arr, 0, arr.length - 1);
+    }
+
+    public Node construct(int[] arr, int start, int end) {
+
+        if(start == end) {
+            Node leaf = new Node(start, end);
+            leaf.data = arr[start];
+            return leaf;
+        }
+
+        Node node = new Node(start, end);
+
+        int mid = start + (end - start) / 2;
+
+        node.left = construct(arr, start, mid);
+        node.right = construct(arr, mid + 1, end);
+
+        node.data = node.left.data + node.right.data;
+        return node;
+    }
+
+    public void display() {
+        display(root);
+    }
+
+    public void display(Node node) {
+
+        if (node != null) {
+            System.out.println("[" + node.startInterval + ", " + node.endInterval + "] => " + node.data);
+
+            display(node.left);
+            display(node.right);
+        }
+    }
+
+
+
+
+}
