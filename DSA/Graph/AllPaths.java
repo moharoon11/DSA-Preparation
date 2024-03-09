a44a92c4710f998d94e70d76512fc038b184d687
moharoon11 <moharoon11107@gmail.com>
2024-03-09 12:46:58 +0530

    Graphs

---
+++ DSA/Graph/AllPaths.java
@@ -0,0 +1,102 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class AllPaths {
+
+    public static void main(String... args) {
+        int v = 7;
+        ArrayList<Edge>[] graph = new ArrayList[v];
+        createGraph(graph);
+        boolean[] visited = new boolean[v];
+        printAllPaths(graph, 0, visited, 5, "0");
+    }
+
+    private static class Edge {
+
+        private int src;
+        private int dest;
+
+        Edge(int src, int dest) {
+            this.src = src;
+            this.dest = dest;
+        }
+    }
+
+
+
+    private static void createGraph(ArrayList<Edge>[] graph) {
+
+        for(int i=0; i<graph.length; i++) {
+            graph[i] = new ArrayList<Edge>();
+        }
+
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 3));
+        graph[1].add(new Edge(1, 0));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+
+        graph[5].add(new Edge(5,3));
+        graph[5].add(new Edge(5,4));
+        graph[5].add(new Edge(5, 6));
+
+        graph[6].add(new Edge(6,5));
+        /*
+        graph[0].add(new Edge(0,2));
+
+        graph[1].add(new Edge(1,2));
+        graph[1].add(new Edge(1,3));
+
+        graph[2].add(new Edge(2,0));
+        graph[2].add(new Edge(2,1));
+        graph[2].add(new Edge(2,3));
+
+        graph[3].add(new Edge(3,1));
+        graph[3].add(new Edge(3,2));
+         **/
+    }
+
+    private static void printAllPaths(ArrayList<Edge>[] graph, int current, boolean[] visited, int target, String path) {
+
+        if(current == target) {
+            System.out.println(path);
+            return;
+        }
+
+        for(int i=0; i<graph[current].size(); i++) {
+            Edge e = graph[current].get(i);
+
+            if(!visited[e.dest]) {
+                visited[current] = true;
+                printAllPaths(graph, e.dest, visited, target, path + e.dest);
+                visited[current] = false;
+            }
+        }
+    }
+
+    private static void dfs(ArrayList<Edge>[] graph, int current, boolean[] visited) {
+        System.out.print(current + " ");
+        visited[current] = true;
+
+        for(int i=0; i<graph[current].size(); i++) {
+            Edge e = graph[current].get(i);
+            if(!visited[e.dest])
+                dfs(graph,e.dest, visited);
+        }
+    }
+
+}
