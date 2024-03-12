becd54d24e72e3687c3aa4c685179728d5ffba47
moharoon11 <104692025+moharoon11@users.noreply.github.com>
2024-03-12 15:46:08 +0530

    Update TopologicalSorting.java

--- DSA/Graph/TopologicalSorting.java
+++ DSA/Graph/TopologicalSorting.java
@@ -6,7 +6,7 @@ public class TopologicalSorting {
 
     public static void main(String... args) {
 
-        int v = 7;
+        int v = 6;
         ArrayList<Edge>[] graph = new ArrayList[v];
         createGraph(graph);
 
