10b0e798da1f50c46b14d436e4c34df9f17bbd6d
moharoon11 <moharoon11107@gmail.com>
2024-05-21 13:02:19 +0530

    slidingwindow

---
+++ DSA/sliding_window/MaxOfAllSubArraySizeK.java
@@ -0,0 +1,42 @@
+package sliding_window;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class MaxOfAllSubArraySizeK {
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {1, 3, -1, -3, 5,3,6,7};
+        System.out.println(find(nums1, 3));
+    }
+
+    public static List<Integer> find(int[] arr, int k) {
+        List<Integer> result = new ArrayList<Integer>();
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>((a, b) ->  b - a);
+
+        int i=0;
+        int j=0;
+
+        while(j < arr.length) {
+
+            // calculations
+            queue.add(arr[j]);
+
+            if(j - i + 1 < k) {
+                j++;
+            } else if(j - i + 1 == k) {
+
+                result.add(queue.peek());
+                queue.remove(arr[i]);
+
+                i++;
+                j++;
+            }
+        }
+
+        return result;
+    }
+}
