7f8f54670c8e70680bb1a69ac74d189d6d1da3e8
moharoon11 <moharoon11107@gmail.com>
2024-05-27 22:05:52 +0530

    arrays

---
+++ DSA/Arrays/LengthOfLongestSubarray.java
@@ -0,0 +1,59 @@
+package Arrays;
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+/*
+Example 1:
+Input Format: N = 3, k = 5, array[] = {2,3,5}
+Result: 2
+Explanation: The longest subarray with sum 5 is {2, 3}. And its length is 2.
+
+Example 2:
+Input Format: N = 5, k = 10, array[] = {2,3,5,1,9}
+Result: 3
+Explanation: The longest subarray with sum 10 is {2, 3, 5}. And its length is 3.
+
+*/
+class LengthOfLongestSubarray {
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,5};
+        int N = 3;
+        int K=5;
+
+        int[] arr1 = {2,3,5,1,9};
+        int N1 = 5;
+        int K1 = 10;
+        System.out.println(find(arr, N, K));
+        System.out.println(find(arr1, N1, K1));
+
+
+    }
+
+    public static int find(int[] arr, int N, int K) {
+        int max = 0;
+
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+
+        while(j < N) {
+
+            sum += arr[j];
+
+            while(i <= j && sum > K) {
+                sum -= arr[i];
+                i++;
+            }
+
+            if(sum == K) {
+                max = Math.max(max, j - i + 1);
+            }
+
+
+            j++;
+        }
+
+        return max;
+    }
+}
