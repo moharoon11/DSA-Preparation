635f9f9ac0d41e1a18b35d66a559f15bf53dee68
moharoon11 <moharoon11107@gmail.com>
2024-05-30 11:44:44 +0530

    sliding window

---
+++ DSA/sliding_window/variable_sliding_window/MaxConsecutiveOnesIII.java
@@ -0,0 +1,53 @@
+package sliding_window.variable_sliding_window;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+public class MaxConsecutiveOnesIII {
+
+    
+    
+    
+    // Brute Force Approach
+    public int longestOnes(int[] arr, int k) {
+
+        int i = 0;
+        int j = 0;
+        int zeroCount = 0;
+        int len = arr.length;
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+        while(j < len) {
+
+            if(arr[j] == 0) {
+                zeroCount++;
+            }
+
+            while(zeroCount > k) {
+                if(arr[i] == 0) {
+                    zeroCount--;
+                }
+                i++;
+            }
+
+            ArrayList<Integer> range = new ArrayList<Integer>();
+            range.add(i);
+            range.add(j);
+            map.put(j - i + 1, range);
+            j++;
+        }
+
+        for(int it:map.keySet()) {
+            max = Math.max(max, it);
+        }
+        return max;
+    }
+
+
+
+    // Sliding Window Approach
+    public int longestOnesSlidingWindow() {
+        int max = 0;
+
+        return max;
+    }
+}
